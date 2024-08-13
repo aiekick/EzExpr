740326b157b656ef343c084ea4e68f0d44cdbfc2
aiekick <aiekick@funparadigm.com>
2024-08-13 23:58:11 +0200

    update license

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,7 +2,7 @@
 /*
 MIT License
 
-Copyright (c) 2024 Aiekick
+Copyright (c) 2024 Stephane Cuillerdier (aka aiekick)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
