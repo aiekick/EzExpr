69ef0fe1c31edfa62eac6b6e9e01c86d2ce8dc13
aiekick <aiekick@funparadigm.com>
2024-08-14 12:30:16 +0200

    removed the perfo test slowdown validation. jsut generate a report

--- tests/EzExpr/perfos/Test_Expr_Perfos.cpp
+++ tests/EzExpr/perfos/Test_Expr_Perfos.cpp
@@ -32,8 +32,8 @@ SOFTWARE.
 #include <fstream>
 
 static bool Bench(const std::string& vEzExpr,
-                  const std::function<double(ez::Expr& vEv)>& vEzExprIteration,
-                  const std::function<double()>& vCppExpr,
+                  const std::function<double(ez::Expr& vEv, double)>& vEzExprIteration,
+                  const std::function<double(int)>& vCppExpr,
                   int vIterations,
                   double slowdownThreshold,
                   double& vOutEzExprTotalTime,
@@ -46,46 +46,47 @@ static bool Bench(const std::string& vEzExpr,
         ez::Expr ev;
         ev.parse(vEzExpr);
 
-        // Accumulation des résultats
         double ezExprAccumulation = 0.0;
         double cppExprAccumulation = 0.0;
+        double tmp;
 
-        // Mesure du temps pour EzExpr
+        // EzExpr Eval
         auto start_ez = std::chrono::high_resolution_clock::now();
-        for (int i = 0; i < vIterations; ++i) {
-            ezExprAccumulation += vEzExprIteration(ev); 
+        for (int j = 0; j < vIterations; ++j) {
+            for (int i = 0; i < vIterations; ++i) {
+                tmp = i;
+                ezExprAccumulation += vEzExprIteration(ev, tmp);
+            }
         }
         auto end_ez = std::chrono::high_resolution_clock::now();
-        vOutEzExprTotalTime = std::chrono::duration<double, std::milli>(end_ez - start_ez).count();
-     //   vOutEzExprAvgTime = vOutEzExprTotalTime / vIterations;
+        vOutEzExprTotalTime = std::floor(std::chrono::duration<double, std::milli>(end_ez - start_ez).count() * 100.0) / 100.0;
 
-        // Mesure du temps pour l'évaluation en C++
+        // Cpp Eval
         auto start_cpp = std::chrono::high_resolution_clock::now();
-        for (int i = 0; i < vIterations; ++i) {
-            cppExprAccumulation += vCppExpr();  
+        for (int j = 0; j < vIterations; ++j) {
+            for (int i = 0; i < vIterations; ++i) {
+                tmp = i;
+                cppExprAccumulation += vCppExpr(tmp);
+            }
         }
         auto end_cpp = std::chrono::high_resolution_clock::now();
-        vOutCppTotalTime = std::chrono::duration<double, std::milli>(end_cpp - start_cpp).count();
-    //    vOutCppAvgTime = vOutCppTotalTime / vIterations;
+        vOutCppTotalTime = std::floor(std::chrono::duration<double, std::milli>(end_cpp - start_cpp).count() * 100.0) / 100.0;
 
-        // Calcul du pourcentage de ralentissement
         double slowdown_total_percentage = ((vOutEzExprTotalTime / vOutCppTotalTime) - 1.0) * 100.0;
-      //  double slowdown_avg_percentage = ((vOutEzExprAvgTime / vOutCppAvgTime) - 1.0) * 100.0;
 
-        // Écriture des résultats dans le flux sous forme de tableau Markdown
         if (outputStreamPtr != nullptr) {
-            (*outputStreamPtr) <<                         //
-                "| " << vEzExpr <<                        //
-                " | " << vOutEzExprTotalTime <<           //
+            (*outputStreamPtr) <<                      //
+                "| " << vEzExpr <<                     //
+                " | " << vOutEzExprTotalTime <<        //
                 " | " << vOutCppTotalTime <<           //
                 " | " << slowdown_total_percentage <<  //
                 "% |\n";
         }
 
-        // Vérification si le ralentissement dépasse le seuil
-        if (slowdown_total_percentage > slowdownThreshold) { 
+        // the test will always been true, the goal is to geenrete a report
+        /*if (slowdown_total_percentage > slowdownThreshold) { 
             return false; 
-        }
+        }*/
 
         // Comparaison des résultats accumulés
         if (std::abs(ezExprAccumulation - cppExprAccumulation) > std::numeric_limits<double>::epsilon()) {
@@ -113,19 +114,9 @@ bool Test_Expr_Perfo_x_squared(double slowdownThreshold, int iterations, std::os
     double cpp_expr_total_time, cpp_expr_avg_time;
 
     return Bench(
-        "x*x",
-        [](ez::Expr& ev) {
-            static double x = 1.0;
-            double result = ev.set("x", x).eval().getResult();
-            x += 0.1; 
-            return result;
-        },
-        []() {
-            static double x = 1.0;
-            double result = x * x;
-            x += 0.1;  
-            return result;
-        },
+        "x*x",  //
+        [](ez::Expr& ev, double t) { return ev.set("x", t).eval().getResult(); },
+        [](double t) { return t * t; },
         iterations,
         slowdownThreshold,
         ez_expr_total_time,
@@ -140,19 +131,9 @@ bool Test_Expr_Perfo_x_cubed(double slowdownThreshold, int iterations, std::ostr
     double cpp_expr_total_time, cpp_expr_avg_time;
 
     return Bench(
-        "x*x*x",
-        [](ez::Expr& ev) {
-            static double x = 1.0;
-            double result = ev.set("x", x).eval().getResult();
-            x += 0.1;
-            return result;
-        },
-        []() {
-            static double x = 1.0;
-            double result = x * x * x;
-            x += 0.1;
-            return result;
-        },
+        "x*x*x",//
+        [](ez::Expr& ev, double t) { return ev.set("x", t).eval().getResult(); },
+        [](double t) { return t * t * t; },
         iterations,
         slowdownThreshold,
         ez_expr_total_time,
@@ -167,19 +148,9 @@ bool Test_Expr_Perfo_sin_x(double slowdownThreshold, int iterations, std::ostrea
     double cpp_expr_total_time, cpp_expr_avg_time;
 
     return Bench(
-        "sin(x)",
-        [](ez::Expr& ev) {
-            static double x = 1.0;
-            double result = ev.set("x", x).eval().getResult();
-            x += 0.1;
-            return result;
-        },
-        []() {
-            static double x = 1.0;
-            double result = std::sin(x);
-            x += 0.1;
-            return result;
-        },
+        "sin(x)",//
+        [](ez::Expr& ev, double t) { return ev.set("x", t).eval().getResult(); },
+        [](double t) { return std::sin(t); },
         iterations,
         slowdownThreshold,
         ez_expr_total_time,
@@ -194,19 +165,9 @@ bool Test_Expr_Perfo_sqrt_x(double slowdownThreshold, int iterations, std::ostre
     double cpp_expr_total_time, cpp_expr_avg_time;
 
     return Bench(
-        "sqrt(x)",
-        [](ez::Expr& ev) {
-            static double x = 1.0;
-            double result = ev.set("x", x).eval().getResult();
-            x += 0.1;
-            return result;
-        },
-        []() {
-            static double x = 1.0;
-            double result = std::sqrt(x);
-            x += 0.1;
-            return result;
-        },
+        "sqrt(x)",//
+        [](ez::Expr& ev, double t) { return ev.set("x", t).eval().getResult(); },
+        [](double t) { return std::sqrt(t); },
         iterations,
         slowdownThreshold,
         ez_expr_total_time,
@@ -221,22 +182,14 @@ bool Test_Expr_Perfo_cos_x_sin_y(double slowdownThreshold, int iterations, std::
     double cpp_expr_total_time, cpp_expr_avg_time;
 
     return Bench(
-        "cos(x) * sin(y)",
-        [](ez::Expr& ev) {
-            static double x = 1.0;
-            static double y = 1.0;
-            double result = ev.set("x", x).set("y", y).eval().getResult();
-            x += 0.1;
-            y += 0.1;
-            return result;
+        "cos(x) * sin(y)",//
+        [](ez::Expr& ev, double t) {
+            double x = t, y = t + 1.0;
+            return ev.set("x", x).set("y", y).eval().getResult();
         },
-        []() {
-            static double x = 1.0;
-            static double y = 1.0;
-            double result = std::cos(x) * std::sin(y);
-            x += 0.1;
-            y += 0.1;
-            return result;
+        [](double t) {
+            double x = t, y = t + 1.0;
+            return std::cos(x) * std::sin(y);
         },
         iterations,
         slowdownThreshold,
@@ -252,26 +205,8 @@ bool Test_Expr_Perfo_a_plus_5(double slowdownThreshold, int iterations, std::ost
     double cpp_expr_total_time, cpp_expr_avg_time;
 
     return Bench(
-        "a+5",
-        [](ez::Expr& ev) {
-            static double a = 1.0;
-            double result = ev.set("a", a).eval().getResult();
-            a += 0.1;
-            return result;
-        },
-        []() {
-            static double a = 1.0;
-            double result = a + 5.0;
-            a += 0.1;
-            return result;
-        },
-        iterations,
-        slowdownThreshold,
-        ez_expr_total_time,
-        ez_expr_avg_time,
-        cpp_expr_total_time,
-        cpp_expr_avg_time,
-        outputStreamPtr);
+        "a+5", //
+        [](ez::Expr& ev, double t) { return ev.set("a", t).eval().getResult(); }, [](double t) { return t + 5.0; }, iterations, slowdownThreshold, ez_expr_total_time, ez_expr_avg_time, cpp_expr_total_time, cpp_expr_avg_time, outputStreamPtr);
 }
 
 bool Test_Expr_Perfo_5_plus_a_plus_5(double slowdownThreshold, int iterations, std::ostream* outputStreamPtr) {
@@ -279,19 +214,9 @@ bool Test_Expr_Perfo_5_plus_a_plus_5(double slowdownThreshold, int iterations, s
     double cpp_expr_total_time, cpp_expr_avg_time;
 
     return Bench(
-        "5+a+5",
-        [](ez::Expr& ev) {
-            static double a = 1.0;
-            double result = ev.set("a", a).eval().getResult();
-            a += 0.1;
-            return result;
-        },
-        []() {
-            static double a = 1.0;
-            double result = 5.0 + a + 5.0;
-            a += 0.1;
-            return result;
-        },
+        "5+a+5",//
+        [](ez::Expr& ev, double t) { return ev.set("a", t).eval().getResult(); },
+        [](double t) { return 5.0 + t + 5.0; },
         iterations,
         slowdownThreshold,
         ez_expr_total_time,
@@ -306,19 +231,9 @@ bool Test_Expr_Perfo_abs_a_plus_5(double slowdownThreshold, int iterations, std:
     double cpp_expr_total_time, cpp_expr_avg_time;
 
     return Bench(
-        "abs(a+5)",
-        [](ez::Expr& ev) {
-            static double a = 1.0;
-            double result = ev.set("a", a).eval().getResult();
-            a += 0.1;
-            return result;
-        },
-        []() {
-            static double a = 1.0;
-            double result = std::abs(a + 5.0);
-            a += 0.1;
-            return result;
-        },
+        "abs(a+5)",//
+        [](ez::Expr& ev, double t) { return ev.set("a", t).eval().getResult(); },
+        [](double t) { return std::abs(t + 5.0); },
         iterations,
         slowdownThreshold,
         ez_expr_total_time,
@@ -333,19 +248,9 @@ bool Test_Expr_Perfo_a_plus_5_times_2(double slowdownThreshold, int iterations, 
     double cpp_expr_total_time, cpp_expr_avg_time;
 
     return Bench(
-        "(a+5)*2",
-        [](ez::Expr& ev) {
-            static double a = 1.0;
-            double result = ev.set("a", a).eval().getResult();
-            a += 0.1;
-            return result;
-        },
-        []() {
-            static double a = 1.0;
-            double result = (a + 5.0) * 2.0;
-            a += 0.1;
-            return result;
-        },
+        "(a+5)*2",//
+        [](ez::Expr& ev, double t) { return ev.set("a", t).eval().getResult(); },
+        [](double t) { return (t + 5.0) * 2.0; },
         iterations,
         slowdownThreshold,
         ez_expr_total_time,
@@ -360,19 +265,9 @@ bool Test_Expr_Perfo_a_plus_5_times_2_alt(double slowdownThreshold, int iteratio
     double cpp_expr_total_time, cpp_expr_avg_time;
 
     return Bench(
-        "a+(5*2)",
-        [](ez::Expr& ev) {
-            static double a = 1.0;
-            double result = ev.set("a", a).eval().getResult();
-            a += 0.1;
-            return result;
-        },
-        []() {
-            static double a = 1.0;
-            double result = a + (5.0 * 2.0);
-            a += 0.1;
-            return result;
-        },
+        "a+(5*2)",//
+        [](ez::Expr& ev, double t) { return ev.set("a", t).eval().getResult(); },
+        [](double t) { return t + (5.0 * 2.0); },
         iterations,
         slowdownThreshold,
         ez_expr_total_time,
@@ -387,19 +282,9 @@ bool Test_Expr_Perfo_sqrt_pow_a(double slowdownThreshold, int iterations, std::o
     double cpp_expr_total_time, cpp_expr_avg_time;
 
     return Bench(
-        "sqrt(pow(a, 1.5) + pow(a, 2.5))",
-        [](ez::Expr& ev) {
-            static double a = 1.0;
-            double result = ev.set("a", a).eval().getResult();
-            a += 0.1;
-            return result;
-        },
-        []() {
-            static double a = 1.0;
-            double result = std::sqrt(std::pow(a, 1.5) + std::pow(a, 2.5));
-            a += 0.1;
-            return result;
-        },
+        "sqrt(pow(a, 1.5) + pow(a, 2.5))",//
+        [](ez::Expr& ev, double t) { return ev.set("a", t).eval().getResult(); },
+        [](double t) { return std::sqrt(std::pow(t, 1.5) + std::pow(t, 2.5)); },
         iterations,
         slowdownThreshold,
         ez_expr_total_time,
@@ -414,19 +299,9 @@ bool Test_Expr_Perfo_complex_fraction(double slowdownThreshold, int iterations, 
     double cpp_expr_total_time, cpp_expr_avg_time;
 
     return Bench(
-        "(1/(a+1)+2/(a+2)+3/(a+3))",
-        [](ez::Expr& ev) {
-            static double a = 1.0;
-            double result = ev.set("a", a).eval().getResult();
-            a += 0.1;
-            return result;
-        },
-        []() {
-            static double a = 1.0;
-            double result = (1.0 / (a + 1.0)) + (2.0 / (a + 2.0)) + (3.0 / (a + 3.0));
-            a += 0.1;
-            return result;
-        },
+        "(1/(a+1)+2/(a+2)+3/(a+3))",//
+        [](ez::Expr& ev, double t) { return ev.set("a", t).eval().getResult(); },
+        [](double t) { return (1.0 / (t + 1.0)) + (2.0 / (t + 2.0)) + (3.0 / (t + 3.0)); },
         iterations,
         slowdownThreshold,
         ez_expr_total_time,
@@ -481,10 +356,10 @@ bool Test_Expr_Perfo_All(double slowdownThreshold, int iterations) {
 
 bool Test_Expr_Perfos_run_test(const std::string& vTest) {
     double slowdownThreshold = 5000.0;  // 50% de ralentissement maximum acceptable
-    int iterations = 10000;
+    int iterations = 1000;
     // Parsing
     IfTestExist(Test_Expr_Perfo_All);
-    else IfTestBenchExist(Test_Expr_Perfo_x_squared);
+    /* else IfTestBenchExist(Test_Expr_Perfo_x_squared);
     else IfTestBenchExist(Test_Expr_Perfo_x_cubed);
     else IfTestBenchExist(Test_Expr_Perfo_sin_x);
     else IfTestBenchExist(Test_Expr_Perfo_sqrt_x);
@@ -495,7 +370,7 @@ bool Test_Expr_Perfos_run_test(const std::string& vTest) {
     else IfTestBenchExist(Test_Expr_Perfo_a_plus_5_times_2);
     else IfTestBenchExist(Test_Expr_Perfo_a_plus_5_times_2_alt);
     else IfTestBenchExist(Test_Expr_Perfo_sqrt_pow_a);
-    else IfTestBenchExist(Test_Expr_Perfo_complex_fraction);
+    else IfTestBenchExist(Test_Expr_Perfo_complex_fraction);*/
     // default
     return false;
 }
